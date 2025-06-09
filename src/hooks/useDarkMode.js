ff0a1b32c137990c56a2ec0f4f2645c046f39d3e
Nishidh <nishidhsingh97@gmail.com>
2025-06-09 14:44:46 +0530

    fix dark mode toggle issue and add default theme to dark

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function useDarkMode() {
-  const [isDark, setIsDark] = useState(true); // Default to true (dark mode)
+  const [isDark, setIsDark] = useState(true); 
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
